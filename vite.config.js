f7bbcdf57d206ae2dce5639a40ce2015ef020bf8
Pranav C R <pranavcr001@gmail.com>
2025-05-28 22:51:02 +0530

    bug fixes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,31 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+    target: 'esnext',
+    minify: 'terser',
+    sourcemap: false
+  },
+  server: {
+    port: 3000
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom'],
+    exclude: ['firebase']
+  },
+  resolve: {
+    alias: {
+      'firebase/app': 'firebase/app',
+      'firebase/auth': 'firebase/auth',
+      'firebase/firestore': 'firebase/firestore'
+    }
+  }
 })
